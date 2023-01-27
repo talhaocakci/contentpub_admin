77f27d3380d8e37cb23c4842e93958e6be5c904c
tocakci <tocakci@twilio.com>
2023-01-27 19:44:34 +0100

    save prices

--- lib/create_product.dart
+++ lib/create_product.dart
@@ -1,58 +1,64 @@
+import 'dart:math';
+
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:contentpub_admin/create_price.dart';
 import 'package:contentpub_admin/file_upload.dart';
+import 'package:contentpub_admin/flutter_flow/flutter_flow_theme.dart';
+import 'package:contentpub_admin/models/Bundle.dart';
+import 'package:contentpub_admin/models/Content.dart';
 import 'package:contentpub_admin/models/ContentType.dart';
+import 'package:contentpub_admin/models/Price.dart';
 import 'package:contentpub_admin/models/PublishProductModel.dart';
 import 'package:contentpub_admin/models/PurchaseType.dart';
+import 'package:contentpub_admin/models/editable/editables.dart';
 import 'package:flutter/material.dart';
 
 import 'dart:html' as html;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:uuid/uuid.dart';
 
-import 'create_bundle.dart';
-
-class EditableContent {
-  final ContentType contentType;
-
-  String? id;
-  String? name;
-  String? description;
-  String? localFile;
-  String? localVideoName;
-  int? localFileSize;
-  String? uploadedFileUrl;
-  String? uploadedVideoAccessUrl;
-  String? uploadMessage;
-  bool? dirty;
-  bool uploadInProgress = false;
-
-  EditableContent({
-    required this.contentType,
-    this.id,
-    this.name,
-    this.description,
-    this.localFile,
-    this.localVideoName,
-    this.localFileSize,
-    this.uploadedFileUrl,
-    this.uploadedVideoAccessUrl,
-    this.uploadMessage,
-    this.dirty,
-    this.uploadInProgress = false,
-  });
-}
+var uuid = Uuid();
 
 class CreateProductWidget extends StatefulWidget {
+  final String bundleId;
+
+  const CreateProductWidget({
+    required this.bundleId,
+    Key? key,
+  }) : super(key: key);
+
   @override
   _CreateProductWidgetState createState() => _CreateProductWidgetState();
 }
 
 class _CreateProductWidgetState extends State<CreateProductWidget> {
-  int _currentStep = 1;
+  int _currentStep = 0;
   StepperType stepperType = StepperType.horizontal;
+
+  late EditablePrice priceOneTime;
+  late EditablePrice priceOneMonth;
+  late EditablePrice priceThreeMonths;
+  late EditablePrice priceOneYear;
 
   final contentTypes = ContentType.values.toList();
 
-  EditableContent? editableContent;
+  EditableBundle? editableBundle;
+
+  @override
+  void initState() {
+    editableBundle = EditableBundle(id: widget.bundleId);
+
+    if (widget.bundleId == '') {
+      priceOneTime = EditablePrice(id: '');
+      priceOneMonth = EditablePrice(id: '');
+      priceThreeMonths = EditablePrice(id: '');
+      priceOneYear = EditablePrice(id: '');
+    }
+
+    super.initState();
+  }
 
   final _formKey = GlobalKey<FormState>();
 
@@ -61,7 +67,7 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Create Bundle to Sell Your Products'),
+        title: const Text('Create Bundle to Sell Your Products'),
         centerTitle: true,
       ),
       body: Container(
@@ -70,7 +76,7 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
             Expanded(
               child: Stepper(
                 type: stepperType,
-                physics: ScrollPhysics(),
+                physics: const ScrollPhysics(),
                 currentStep: _currentStep,
                 onStepTapped: (step) => tapped(step),
                 onStepContinue: continued,
@@ -83,10 +89,27 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
                         child: Column(
                           children: <Widget>[
                             TextFormField(
+                              onSaved: (newValue) {
+                                editableBundle!.name = newValue;
+                              },
+
                               // The validator receives the text that the user has entered.
                               validator: (value) {
                                 if (value == null || value.isEmpty) {
-                                  return 'Please enter some text';
+                                  return 'Please enter the name';
+                                }
+                                return null;
+                              },
+                            ),
+                            TextFormField(
+                              onSaved: (newValue) {
+                                editableBundle!.description = newValue;
+                              },
+
+                              // The validator receives the text that the user has entered.
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter the description';
                                 }
                                 return null;
                               },
@@ -97,9 +120,25 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
                                 if (_formKey.currentState!.validate()) {
                                   // If the form is valid, display a snackbar. In the real world,
                                   // you'd often call a server or save the information in a database.
+
+                                  Bundle bundle = EditableBundle.fromEditable(
+                                      editableBundle!);
+
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     const SnackBar(
-                                        content: Text('Processing Data')),
+                                        content: Text('Saving the product')),
+                                  );
+
+                                  saveBundle(bundle);
+
+                                  _currentStep++;
+
+                                  setState(() {
+                                    _currentStep = _currentStep;
+                                  });
+
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text('Saved')),
                                   );
                                 }
                               },
@@ -108,27 +147,86 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
                             // Add TextFormFields and ElevatedButton here.
                           ],
                         )),
-                    isActive: _currentStep >= 0 && editableContent != null,
-                    state: _currentStep >= 1 &&
-                            editableContent?.uploadedFileUrl != null
+                    isActive: _currentStep >= 0 && editableBundle != null,
+                    state: _currentStep >= 1 && editableBundle?.name != null
                         ? StepState.complete
                         : StepState.disabled,
                   ),
                   Step(
                     title: new Text('Prices'),
-                    content: Column(
-                      children: <Widget>[
-                        ElevatedButton(
-                          onPressed: () {
-                            publishProduct();
-                          },
-                          child: Text('Publish'),
-                        ),
-                      ],
+                    content: SizedBox(
+                      height: 300,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            child: DefaultTabController(
+                              length: 4,
+                              initialIndex: 0,
+                              child: Column(
+                                children: [
+                                  TabBar(
+                                    labelColor: FlutterFlowTheme.of(context)
+                                        .primaryColor,
+                                    labelStyle:
+                                        FlutterFlowTheme.of(context).bodyText1,
+                                    indicatorColor: FlutterFlowTheme.of(context)
+                                        .secondaryColor,
+                                    tabs: const [
+                                      Tab(
+                                        text: 'One time',
+                                      ),
+                                      Tab(
+                                        text: '1 month',
+                                      ),
+                                      Tab(
+                                        text: '3 months',
+                                      ),
+                                      Tab(
+                                        text: '1 year',
+                                      ),
+                                    ],
+                                  ),
+                                  Expanded(
+                                    child: TabBarView(
+                                      children: [
+                                        SizedBox(
+                                            width: 100,
+                                            child: CreatePriceWidget(
+                                                bundleId: widget.bundleId,
+                                                price: priceOneTime)),
+                                        SizedBox(
+                                            width: 100,
+                                            child: CreatePriceWidget(
+                                                bundleId: widget.bundleId,
+                                                price: priceOneMonth)),
+                                        SizedBox(
+                                            width: 100,
+                                            child: CreatePriceWidget(
+                                                bundleId: widget.bundleId,
+                                                price: priceThreeMonths)),
+                                        SizedBox(
+                                            width: 100,
+                                            child: CreatePriceWidget(
+                                                bundleId: widget.bundleId,
+                                                price: priceOneYear)),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                savePrices();
+                              },
+                              child: const Text('Save prices'))
+                        ],
+                      ),
                     ),
-                    isActive: _currentStep >= 0 && editableContent != null,
-                    state: _currentStep >= 2 &&
-                            editableContent?.uploadedFileUrl != null
+                    isActive: _currentStep >= 0 && editableBundle != null,
+                    state: _currentStep >= 2 && editableBundle?.prices != null
                         ? StepState.complete
                         : StepState.disabled,
                   ),
@@ -140,13 +238,13 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
                           onPressed: () {
                             publishProduct();
                           },
-                          child: Text('Publish'),
+                          child: const Text('Contents'),
                         ),
                       ],
                     ),
-                    isActive: _currentStep >= 0 && editableContent != null,
-                    state: _currentStep >= 2 &&
-                            editableContent?.uploadedFileUrl != null
+                    isActive:
+                        _currentStep >= 0 && editableBundle!.contents != null,
+                    state: _currentStep >= 2 && editableBundle?.contents != null
                         ? StepState.complete
                         : StepState.disabled,
                   ),
@@ -158,13 +256,12 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
                           onPressed: () {
                             publishProduct();
                           },
-                          child: Text('Publish'),
+                          child: const Text('Publish'),
                         ),
                       ],
                     ),
-                    isActive: _currentStep >= 0 && editableContent != null,
-                    state: _currentStep >= 2 &&
-                            editableContent?.uploadedFileUrl != null
+                    isActive: _currentStep >= 0 && editableBundle != null,
+                    state: _currentStep >= 2 && editableBundle?.prices != null
                         ? StepState.complete
                         : StepState.disabled,
                   )
@@ -175,7 +272,7 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.list),
+        child: const Icon(Icons.list),
         onPressed: switchStepsType,
       ),
     );
@@ -197,6 +294,41 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
 
   cancel() {
     _currentStep > 0 ? setState(() => _currentStep -= 1) : null;
+  }
+
+  savePrice(EditablePrice editablePrice) async {
+    Price price;
+    if (editablePrice.amount != null) {
+      if (editablePrice.id == '') {
+        editablePrice.id = uuid.v4();
+        price = EditablePrice.fromEditable(editablePrice);
+        final priceSaveRequest = ModelMutations.create(price);
+        var response =
+            await Amplify.API.mutate(request: priceSaveRequest).response;
+        print(response.data);
+        print(response.errors);
+      } else {
+        price = EditablePrice.fromEditable(editablePrice);
+        final priceSaveRequest = ModelMutations.update(price);
+        var response =
+            await Amplify.API.mutate(request: priceSaveRequest).response;
+        print(response.data);
+        print(response.errors);
+      }
+
+      print(price.id);
+      editablePrice.id = price.id;
+    }
+  }
+
+  savePrices() async {
+    savePrice(priceOneTime);
+    savePrice(priceOneMonth);
+    savePrice(priceThreeMonths);
+    savePrice(priceOneYear);
+
+    _currentStep++;
+    setState(() {});
   }
 
   void publishProduct() async {
@@ -246,3 +378,66 @@ class _CreateProductWidgetState extends State<CreateProductWidget> {
     print(p.stripeProductId);
   }
 }
+
+saveBundle(Bundle bundle) async {
+  final bundleSaveRequest = ModelMutations.create(bundle);
+  await Amplify.API.mutate(request: bundleSaveRequest);
+
+  print(bundle.id);
+}
+
+
+Future<List<Content>?> getContents() async {
+    const listContents = 'listContents';
+
+    String graphQLQuery = '''query GetContents {
+              listContents {
+                items {
+                  id
+                  name
+                  objectId
+                  owner
+                  s3Url
+                  description
+                  type
+                  createdAt
+                  updatedAt
+                  coworkers {
+                    items {
+                      id
+                      coworkerId
+                    }
+                  }
+                }
+              }
+            }
+    ''';
+
+    final request = GraphQLRequest(
+        document: graphQLQuery,
+        modelType: const PaginatedModelType(Content.classType),
+        variables: <String, String>{},
+        decodePath: listContents);
+
+    try {
+      var response = await Amplify.API.query(request: request).response;
+      PaginatedResult<Content> result = response.data;
+      var retrievedContents = result.items;
+      for (var content in contents) {
+        print(content);
+      }
+
+      retrievedContents.sort((a, b) =>
+          (b!.updatedAt ?? TemporalDateTime(DateTime.now()))
+              .compareTo(a!.updatedAt ?? TemporalDateTime(DateTime.now())));
+
+      retrievedContents.forEach((element) {
+        print(element!.updatedAt);
+      });
+
+      setState(() {
+        contents = retrievedContents;
+      });
+    } catch (e) {
+      print(e);
+    }
