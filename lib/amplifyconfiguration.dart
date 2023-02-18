5a538045fa096486b3ea3d34c20ab8e575d93430
tocakci <tocakci@twilio.com>
2023-02-18 22:17:50 +0100

    amplify configuration template

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,88 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "javathlon": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://::graphqlId.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "::graphqlApiKey"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://::graphqlId.appsync-api.us-east-1.amazonaws.com/graphql",
+                        "Region": "us-east-1",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "::AppSyncApiKey",
+                        "ClientDatabasePrefix": "::AppSyncClientDatabasePrefix"
+                    }
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:::CredentialsProviderPoolId",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "::PoolId",
+                        "AppClientId": "::AppClientId",
+                        "AppClientSecret": "::AppClientSecret",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "OAuth": {
+                            "WebDomain": "::WebDomain",
+                            "AppClientId": "::AppClientId",
+                            "AppClientSecret": "::AppClientSecret",
+                            "SignInRedirectURI": "::SignInRedirectURI",
+                            "SignOutRedirectURI": "::SignOutRedirectURI",
+                            "Scopes": [
+                                "email",
+                                "openid"
+                            ]
+                        },
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": [
+                                "REQUIRES_LOWERCASE",
+                                "REQUIRES_UPPERCASE",
+                                "REQUIRES_NUMBERS",
+                                "REQUIRES_SYMBOLS"
+                            ]
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
