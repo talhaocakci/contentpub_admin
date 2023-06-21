51513c95712b753998951b0cb02a3ba8fde2e7bd
talhaocakci <btocakci@gmail.com>
2023-06-21 16:46:34 +0200

    Betterments o readme

--- initstack.sh
+++ initstack.sh
@@ -17,45 +17,9 @@ terraform init
 
 terraform apply -var-file="staging.tfvars" -var="project_name=$projectname" -var="aws_profile=$awsprofilename"
 
-# go back to root folder and init amplify there
-cd ..
-cd ..
-
-echo "{\"project\":\"$projectname\"}" >> assets/projectconfiguration.json
-
-amplify init
-
-amplify add api
-
-# get the name before running the shell, use it here instead of javathlon
-
-rm amplify/backend/api/$projectname/schema.graphql
-cp schema.graphql amplify/backend/api/$projectname/schema.graphql
-
-amplify push
 
 
 
-##prints graphql instances, this should be written back into a file
-
-# run model generation here again
-# get pull command first
-
-amplify codegen models
-
-
-amplify import auth
-
-amplify push
-
-
-amplify pull
-
-export AWS_PROFILE=put_your_profile_name_here
-
-flutter build web --release
-
-cp -a build/web/. terraform/deployment/artifacts/
 
 ## run in terraform/deployment folder
 terraform apply -var="project_name=$projectname"
